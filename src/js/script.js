222cbd499a14d7462b5e29e5a5b918d0996df1ff
Matthieu Schneider <matthieu.snd@gmail.com>
2019-09-04 10:58:51 +0200

    [📦] New features : live reload Ouuuuh AND bugs fixed : watch in parallels, etc...

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 
 function component() {
+    console.log('JS component!');
     let element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['here', 'some', 'script!'], ' ');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/js/script.js',
+    mode: 'development',
     output: {
         filename: 'script-bundled.js',
         path: path.resolve(__dirname, 'dist/js')
